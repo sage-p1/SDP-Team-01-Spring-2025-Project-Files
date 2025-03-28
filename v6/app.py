5af4fd434bd9baab296f7ef4b9136c79ddf9eda7
yarid20006 <yarid.tyran@uconn.edu>
2025-03-27 23:55:43 -0400

    Add v6 folder copied from v5 in kaitlyn branch

--- v6/app.py
+++ v6/app.py
@@ -281,5 +281,10 @@ def video():
 def references():
     return render_template("references.html")
 
+@app.route("/upload", methods=["GET", "POST"])
+def upload():
+    return render_template("upload_data.html")
+
+
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=5000, debug=True)
